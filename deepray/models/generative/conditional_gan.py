b2ec4bd997c1080e1f7afca5734168bab0814860
fuhailin <hailinfufu@outlook.com>
2023-08-09 14:54:39 +0800

    Update

--- deepray/models/generative/conditional_gan.py
+++ deepray/models/generative/conditional_gan.py
@@ -49,14 +49,13 @@ pip install -q git+https://github.com/tensorflow/docs
 ## Imports
 """
 
+import imageio
+import numpy as np
+import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
-
 from tensorflow_docs.vis import embed
-import matplotlib.pyplot as plt
-import tensorflow as tf
-import numpy as np
-import imageio
+
 """
 ## Constants and hyperparameters
 """
