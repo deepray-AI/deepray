da10bf95fcd536c4d00521715680d8624ad33ff5
Vincent <hailinfufu@outlook.com>
2023-10-31 11:27:00 +0800

    set jemalloc in docker

--- tools/docker/bashrc.bash
+++ tools/docker/bashrc.bash
@@ -6,3 +6,6 @@ export PATH="$CUDA_HOME/bin:$PATH"
 export LD_LIBRARY_PATH="$CUDA_HOME/lib64:$LD_LIBRARY_PATH"
 
 export LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu/:$LD_LIBRARY_PATH
+
+# Enable jemalloc to optimize memory usage
+export LD_PRELOAD="/usr/lib/x86_64-linux-gnu/libjemalloc.so" 
