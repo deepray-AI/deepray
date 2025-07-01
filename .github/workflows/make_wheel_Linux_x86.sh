ae69b887eab052ee5ec74651cee3ac9c0e5b6ed4
fuhailin <hailinfufu@outlook.com>
2025-07-01 14:26:31 +0800

    CI: Update make_heel_Linux_x86.sh

--- .github/workflows/make_wheel_Linux_x86.sh
+++ .github/workflows/make_wheel_Linux_x86.sh
@@ -7,8 +7,8 @@ docker info
 DOCKER_BUILDKIT=1 docker build \
     -f tools/docker/build_wheel.Dockerfile \
     --output type=local,dest=wheelhouse \
-    --build-arg PY_VERSION \
-    --build-arg TF_VERSION \
-    --build-arg NIGHTLY_FLAG \
-    --build-arg NIGHTLY_TIME \
+    --build-arg PY_VERSION=${PY_VERSION} \
+    --build-arg TF_VERSION=${TF_VERSION} \
+    --build-arg NIGHTLY_FLAG=${NIGHTLY_FLAG} \
+    --build-arg NIGHTLY_TIME=${NIGHTLY_TIME} \
     ./
