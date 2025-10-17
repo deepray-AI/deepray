b3bbccc70a8bbbbac5626edc2eb709eee9c7e448
fuhailin <hailinfufu@outlook.com>
2025-10-17 23:39:58 +0800

    [CI] Add libtool for ucx building

--- tools/install_deps/install_openmpi.sh
+++ tools/install_deps/install_openmpi.sh
@@ -26,7 +26,7 @@ export OMPI_DIR=${INSTALL_DIR}/openmpi
 
 apt-get update &&
     apt-get install --no-install-recommends --yes \
-        wget build-essential autoconf
+        wget build-essential autoconf libtool
 
 # Install UCX
 mkdir /tmp/ucx && cd /tmp/ucx
